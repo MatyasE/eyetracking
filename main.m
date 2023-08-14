d6d73d52de9cba3960e55bb613709f2a0104ba34
MatyasE <56436220+MatyasE@users.noreply.github.com>
2023-08-14 23:57:47 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,4 @@
-clear all
-
+%this file presents a prepared configuration for analysis of sample video
 filename='video.mp4';
 startT=0;
 endT=19;
@@ -7,12 +6,15 @@ bidirer=6;
 rot=[1.7 5.2 8.8 11.7 15.2 17.6];
 texts={'head rot. right', 'head rot. left','eyes to right','eyes to left','eyes to right','eyes to left'} %, 'head rot. right','eyes to right','eyes to left' 
 
+
+%%detect eigenfeatures in ROI
 fs=VideoReader(filename).FrameRate;
 [pointsallX, pointsallY]=detectfeaturesinROI(filename,startT,endT,bidirer);
 points=[pointsallX(:,end) pointsallY(:,end)];
 fig=gcf;
 sizefig=fig.Position(3:4);
 
+%%highlight required eigenfeatures
 indl=higlightfeaturesclosetoPOI(sizefig,points,"left eye pupil");
 indfl=higlightfeaturesclosetoPOI(sizefig,points,"left medial commisure");
 indfr=higlightfeaturesclosetoPOI(sizefig,points,"right medial commisure");
@@ -30,3 +32,4 @@ replayPOI(filename,startT,endT,pointsallX,pointsallY,indl,indfl,indfr,indr);
 %%plot it
 time=linspace(0,length(pointsLeft)/fs,length(pointsLeft));
 plotmovements(time,fs,pointsLeft,pointsFixedLeft,pointsFixedRight,pointsRight,rot,texts);
+
